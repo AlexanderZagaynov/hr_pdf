00596d39f4ee5587127037cb711a0b06bc66ecbc
Alexander Zagaynov <zalex80@gmail.com>
2016-06-30 03:44:07 +0300

    conversion controller

---
+++ app/controllers/conversion_controller.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class ConversionController < ApplicationController
+  LO_EXE = ENV.fetch('LO_EXE') { '/Applications/LibreOffice.app/Contents/MacOS/soffice' }
+  WORK_DIR = Rails.root / 'tmp'
+  XML_FILE = WORK_DIR / 'content.xml'
+
+  %w(a e i o u).tap do |subs|
+    RSUBS = Regexp.new "[#{subs.join ''}]"
+    XSUBS = "//*[#{subs.map { |text| %Q[contains(text(), "#{text}")] }.join(' or ')}]"
+  end
+  REPLACEMENT = 'x'
+
+  def create
+    original = params[:file]
+    redirect_to action: :new and return if original == ''
+
+    filename = original.original_filename
+    filename = "#{filename}#{File.extname filename}"
+    filename = WORK_DIR / filename
+    File.open(filename, 'wb') { |file| file.write original.read }
+
+    barename = File.basename filename, '.*'
+    odt_file = WORK_DIR / "#{barename}.odt"
+    pdf_file = WORK_DIR / "#{barename}.pdf"
+
+    `#{LO_EXE} --invisible --convert-to odt:writer8 --outdir #{WORK_DIR} '#{filename}'`
+    `unzip '#{odt_file}' content.xml -d #{WORK_DIR}`
+
+    has_changes = false
+
+    doc = Nokogiri::XML XML_FILE.read
+    doc.xpath(XSUBS).each do |node|
+      node.content = node.text.gsub RSUBS, REPLACEMENT
+      has_changes ||= true
+    end
+
+    if has_changes
+      XML_FILE.open('w') { XML_FILE.write doc.to_xml }
+      `zip -mj '#{odt_file}' #{XML_FILE}`
+    end
+
+    `#{LO_EXE} --headless --convert-to pdf --outdir #{WORK_DIR} '#{odt_file}'`
+    send_data pdf_file.read, filename: File.basename(pdf_file)
+
+  ensure
+    [XML_FILE, odt_file, pdf_file].each do |file|
+      FileUtils.rm file if File.exist? file
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  controller :conversion do
+    get  '', action: :new, as: :conversion
+    post '', action: :create
+  end
 end
